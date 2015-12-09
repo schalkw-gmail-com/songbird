de2dc9497b2820f68bee8e2ff2c5a47b98f984eb
Bernard Peh <bernard.peh@cheproximity.com.au>
2015-12-09 14:25:44 +1100

    add password reset functionality

--- www/songbird/src/AppBundle/Event/UserCustomAction.php
+++ www/songbird/src/AppBundle/Event/UserCustomAction.php
@@ -4,9 +4,26 @@ namespace AppBundle\Event;
 
 use Sonata\AdminBundle\Event\ConfigureEvent;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\DependencyInjection\Container;
 
 class UserCustomAction
 {
+    /**
+     * @var Container
+     */
+    private $container;
+
+    /**
+     * Constructor
+     *
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
 
     /**
      * show an error if user is not superadmin and tries to manage other user details
@@ -21,11 +38,26 @@ class UserCustomAction
             return;
         }
 
-        $user_id = $event->getAdmin()->getRequest()->attributes->all()['id'];
-        // we can get container from getAdmin
-        $session_id = $event->getAdmin()->getConfigurationPool()->getContainer()->get('security.context')->getToken()->getUser()->getId();
+        // $user_id = $event->getAdmin()->getRequest()->attributes->all()['id'];
+        // $session_id = $event->getAdmin()->getConfigurationPool()->getContainer()->get('security.context')->getToken()->getUser()->getId();
+        $user_id = $this->container->get('request')->attributes->all()['id'];
+        $session_id = $this->container->get('security.context')->getToken()->getUser()->getId();
+        
         if ($user_id != $session_id) {
             throw new AccessDeniedException();
         }
     }
+
+    /**
+     * Redirect user to another page after password reset is success
+     * 
+     * @param  Configure $event GetResponseUserEvent
+     * @return null
+     */
+    public function redirectUserAfterPasswordReset(FormEvent $event)
+    {
+        $url = $this->container->get('router')->generate('sonata_admin_dashboard');
+        $event->setResponse(new RedirectResponse($url));
+    }
 }
+
