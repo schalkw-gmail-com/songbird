82427d1aebcc223193ba686816d2c5e73c1bc5d0
bernard peh <bpeh@sitecritic.net>
2015-12-19 21:32:57 +1100

    mass updates

---
+++ www/songbird/gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var less = require('gulp-less');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var exec = require('child_process').exec;
+
+// Minify JS
+gulp.task('js', function () {
+    return gulp.src(['bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js'])
+        .pipe(concat('javascript.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('web/minified/js'));
+});
+
+// Minify CSS
+gulp.task('css', function () {
+    return gulp.src([
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'src/AppBundle/Resources/public/less/*.less',
+        'src/AppBundle/Resources/public/sass/*.scss',
+        'src/AppBundle/Resources/public/css/*.css'])
+        .pipe(gulpif(/[.]less/, less()))
+        .pipe(gulpif(/[.]scss/, sass()))
+        .pipe(concat('styles.css'))
+        .pipe(uglifycss())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('web/minified/css'));
+});
+
+// Copy Fonts
+gulp.task('fonts', function() {
+    return gulp.src('bower_components/bootstrap/fonts/*.{ttf,woff,woff2,eof,svg}')
+    .pipe(gulp.dest('web/minified/fonts'));
+});
+
+gulp.task('watch', function () {
+    var onChange = function (event) {
+        console.log('File '+event.path+' has been '+event.type);
+    };
+    gulp.watch('src/AppBundle/Resources/public/js/*.js', ['js'])
+        .on('change', onChange);
+
+    gulp.watch('src/AppBundle/Resources/public/less/*.less', ['css'])
+        .on('change', onChange);
+
+    gulp.watch('src/AppBundle/Resources/public/sass/*.scss', ['css'])
+        .on('change', onChange);
+
+    gulp.watch('src/AppBundle/Resources/public/css/*.css', ['css'])
+        .on('change', onChange);
+});
+
+gulp.task('installAssets', function() {
+    exec('./scripts/assetsinstall', logStdOutAndErr);
+});
+// show exec output
+var logStdOutAndErr = function (err, stdout, stderr) {
+    console.log(stdout + stderr);
+};
+//define executable tasks when running "gulp" command
+gulp.task('default', ['js', 'css', 'fonts', 'installAssets', 'watch']);
