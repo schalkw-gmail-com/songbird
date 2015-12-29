6df9791a77c6a9cdf580d25a1456a06b0d6afe10
bernard peh <bpeh@sitecritic.net>
2015-12-30 09:20:43 +1100

    snapshot of tests

---
+++ www/songbird/src/Songbird/NestablePageBundle/tests/acceptance/As_An_Admin/IWantToManagePagesCest.php
@@ -0,0 +1,19 @@
+<?php
+namespace As_An_Admin;
+use \AcceptanceTester;
+
+class IWantToManagePagesCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
