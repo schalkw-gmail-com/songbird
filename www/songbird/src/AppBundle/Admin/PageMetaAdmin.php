30629f7b9c1bc4934011e2bb8effa173ab118c13
bernard peh <bpeh@sitecritic.net>
2016-01-09 12:37:38 +1100

    massive update of files

--- www/songbird/src/AppBundle/Admin/PageMetaAdmin.php
+++ www/songbird/src/AppBundle/Admin/PageMetaAdmin.php
@@ -10,70 +10,21 @@ use Sonata\AdminBundle\Show\ShowMapper;
 
 class PageMetaAdmin extends Admin
 {
-    /**
-     * @param DatagridMapper $datagridMapper
-     */
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        $datagridMapper
-            ->add('id')
-            ->add('page_title')
-            ->add('menu_title')
-            ->add('locale')
-            ->add('short_description')
-            ->add('content')
-        ;
-    }
 
-    /**
-     * @param ListMapper $listMapper
-     */
-    protected function configureListFields(ListMapper $listMapper)
-    {
-        $listMapper
-            ->add('id')
-            ->add('page_title')
-            ->add('menu_title')
-            ->add('locale')
-            ->add('short_description')
-            ->add('content')
-            ->add('_action', 'actions', array(
-                'actions' => array(
-                    'show' => array(),
-                    'edit' => array(),
-                    'delete' => array(),
-                )
-            ))
-        ;
-    }
-
+    protected $baseRouteName = 'admin_pagemeta';
+    
     /**
      * @param FormMapper $formMapper
      */
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('id')
-            ->add('page_title')
             ->add('menu_title')
-            ->add('locale')
-            ->add('short_description')
-            ->add('content')
-        ;
-    }
-
-    /**
-     * @param ShowMapper $showMapper
-     */
-    protected function configureShowFields(ShowMapper $showMapper)
-    {
-        $showMapper
-            ->add('id')
             ->add('page_title')
-            ->add('menu_title')
-            ->add('locale')
-            ->add('short_description')
-            ->add('content')
-        ;
+            ->add('locale', 'locale')
+            ->add('short_description', 'text', array(
+                'required' => false))
+            ->add('content', 'textarea', array(
+                'required' => false));
     }
 }
