a17ab945996970597686eca9c80d9937e40a2a2b
bernard peh <bpeh@sitecritic.net>
2016-01-15 22:56:31 +1100

    update bdd

--- www/songbird/src/AppBundle/tests/acceptance/As_Test1_User/IDontWantToManagePagesCest.php
+++ www/songbird/src/AppBundle/tests/acceptance/As_Test1_User/IDontWantToManagePagesCest.php
@@ -1,6 +1,7 @@
 <?php
 namespace As_Test1_User;
 use \AcceptanceTester;
+use \Common;
 
 class IDontWantToManagePagesCest
 {
@@ -12,8 +13,38 @@ class IDontWantToManagePagesCest
     {
     }
 
-    // tests
-    public function tryToTest(AcceptanceTester $I)
+    protected function login(AcceptanceTester $I)
     {
+        Common::login($I, TEST1_USERNAME, TEST1_PASSWORD);
+    }
+
+    /**
+     * Scenario 19.21
+     * @before login
+     */
+    public function listPages(AcceptanceTester $I)
+    {
+        $I->amOnPage('/admin/app/page/list');
+        $I->canSee('Access Denied');
+    }
+
+    /**
+     * Scenario 19.22
+     * @before login
+     */
+    public function showAboutUsPage(AcceptanceTester $I)
+    {
+        $I->amOnPage('/admin/app/page/2/show');
+        $I->canSee('Access Denied');
+    }
+
+
+     /**
+     * Scenario 19.23
+     * @before login
+     */
+    public function editAboutUsPage(AcceptanceTester $I) {
+        $I->amOnPage('/admin/app/page/2/edit');
+        $I->canSee('Access Denied');
     }
 }
